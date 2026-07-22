c9b63e883c8e16d560f743fe66e3fcc8fd5cc5cc
christiantesamty-jpg <christian.tesamty@gmail.com>
2026-07-22 15:08:25 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE = "confin-v3.3";
+const CACHE = "confin-v3.4";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=3.3",
-  "./app.js?v=3.3",
+  "./styles.css?v=3.4",
+  "./app.js?v=3.4",
   "./manifest.webmanifest",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
